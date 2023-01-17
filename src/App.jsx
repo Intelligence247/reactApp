47b133d55715f3dd96e843866009d5b785a2d062
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-18 00:55:07 +0100

    reactApp

---
+++ src/App.jsx
@@ -0,0 +1,144 @@
+// import logo from './logo.svg';
+
+import './App.css';
+function App() {
+  return (
+    <div className="body">
+      {/* <img src={logo} alt="" /> */}
+      <header>
+        <p>Logo</p>
+        <div className="buttons">
+          <p>Button 1</p>
+          <p>Button 2</p>
+          <p>Button 3</p>
+          <p>Button 4</p>
+        </div>
+        <div className="rightbuttons">
+          <p>Button 1</p>
+          <p>Button 2</p>
+        </div>
+      </header>
+
+
+
+      <div className="welcome">
+        <div className="heading">Welcome to the best website template!</div>
+        <p>Are you looking for a solution to your problem? Look no further, because we have the answer. Our team of experts has the skills and experience to deliver top-quality results.</p>
+        <div className="btn">
+          <p>Button 1</p>
+          <p>Button 2</p>
+        </div>
+      </div>
+
+      <div className="twopics">
+        <img src="./media/firstimg.png" alt="" />
+        <img src="./media/handsdown.png" alt="" />
+
+      </div>
+      <div className="headersInterface">
+        <div className="header1">        <div className="vectorimg">
+          <img src="./media/vector1.png" alt="" />
+          <p>Header</p>
+        </div>
+          <div className="texts">
+            Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
+          </div>
+          <div className="buttonGreater">
+            <p>Button</p>
+            <img src="./media/greater.png" alt="" />
+          </div>
+        </div><div className="header1">        <div className="vectorimg">
+          <img src="./media/vector1.png" alt="" />
+          <p>Header</p>
+        </div>
+          <div className="texts">
+            Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
+          </div>
+          <div className="buttonGreater">
+            <p>Button</p>
+            <img src="./media/greater.png" alt="" />
+          </div>
+        </div><div className="header1">        <div className="vectorimg">
+          <img src="./media/vector1.png" alt="" />
+          <p>Header</p>
+        </div>
+          <div className="texts">
+            Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
+          </div>
+          <div className="buttonGreater">
+            <p>Button</p>
+            <img src="./media/greater.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="reason">
+        <div className="why">
+          <p>Why you should choose us</p>
+          <p>Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background</p>
+          <p>Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background</p>
+        </div>
+        <div className="whypics">
+          <img src="./media/art1.png" alt="" />
+          <img src="./media/art2.png" alt="" />
+        </div>
+      </div>
+
+
+      <div className="reason">
+        <div className="why">
+          <p>Why you should choose us</p>
+          <img src="./media/flowerimg.png" alt="" />
+        </div>
+        <div className="why">
+          <p>Subheader</p>
+          <p>Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background</p>
+          <p>Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background</p>
+        </div>
+      </div>
+
+
+
+      <div className="gallery">
+        <div className="handdownimg">
+          <img src="./media/handsdown.png" alt="" />
+
+        </div>
+        <div className="imgs4">
+          <img src="./media/cameraimg.png" alt="" />
+          <img src="./media/secondimg.png" alt="" />
+          <img src="./media/thirdimg.png" alt="" />
+          <img src="./media/fourthimg.png" alt="" />
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
